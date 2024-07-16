32dbe1f8b1f23717045f1f94b72c95b7d56927f9
Camila Villalba <cami_vp_23@hotmail.com>
2024-07-16 20:00:33 +0200

    add behaviour to button actions

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,28 @@
-import "./style.css";
+const buttonBack = document.getElementById("button-back");
+const buttonForward = document.getElementById("button-forward");
+const buttonReset = document.getElementById("button-reset");
+const currentTurn = document.getElementById("current-turn") as HTMLElement;
 
-console.log((120-18) / 6);
+function goBack() {
+  const value = parseInt(currentTurn.innerHTML as string);
+  const finalNumber = value - 1;
+  const finalNumberConverted = finalNumber.toString();
 
+  if (finalNumber >= 0) {
+    currentTurn.innerHTML = finalNumberConverted.padStart(2,"0");
+  } 
+}
+function goForward() {
+  const value = parseInt(currentTurn.innerHTML as string);
+  const finalNumber = value + 1;
+  const finalNumberConverted = finalNumber.toString();
+  currentTurn.innerHTML = finalNumberConverted.padStart(2,"0");
+}
+
+function resetTurn() {
+  currentTurn.innerHTML = "00";
+}
+
+buttonBack?.addEventListener("click", goBack);
+buttonForward?.addEventListener("click", goForward);
+buttonReset?.addEventListener("click", resetTurn);
