42a4cdb2454f22e81498b4ea7cd15aede488c5a4
Camila Villalba <cami_vp_23@hotmail.com>
2024-07-17 22:03:28 +0200

    change turn action

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ const buttonBack = document.getElementById("button-back");
 const buttonForward = document.getElementById("button-forward");
 const buttonReset = document.getElementById("button-reset");
 const currentTurn = document.getElementById("current-turn") as HTMLElement;
+const newTurn = document.getElementById("custom-turn") as HTMLInputElement;
+const buttonChangeTurn = document.getElementById("button-custom-turn");
+
 
 function goBack() {
   const value = parseInt(currentTurn.innerHTML as string);
@@ -23,6 +26,21 @@ function resetTurn() {
   currentTurn.innerHTML = "00";
 }
 
+function changeTurn() {
+ const value = parseInt(newTurn.value);
+
+ if (value > 0) {
+ const finalValue = value.toString();
+ currentTurn.innerHTML = finalValue.padStart(2,"0")
+
+ } else {
+  currentTurn.innerHTML = "00";
+ }
+ 
+}
+
 buttonBack?.addEventListener("click", goBack);
 buttonForward?.addEventListener("click", goForward);
 buttonReset?.addEventListener("click", resetTurn);
+buttonChangeTurn?.addEventListener("click", changeTurn);
+
